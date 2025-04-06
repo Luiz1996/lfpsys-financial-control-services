a6c6a22fc29ec572350c9ccf551d6c808c9fbba9
Luiz1996 <luiz_flavio_p@hotmail.com>
2025-04-06 12:51:37 -0300

    Initial commit.

---
+++ src/main/java/com/lfpsys/lfpsys_financial_control_services/LfpsysFinancialControlServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lfpsys.lfpsys_financial_control_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpsysFinancialControlServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpsysFinancialControlServicesApplication.class, args);
+	}
+
+}
